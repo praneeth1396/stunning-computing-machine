bbeffb2ffad889a3b5dd305d949d04007fd7412d
Narasimha Praneeth <Narasimha Praneeth>
2016-06-19 21:36:56 +0530

    My project

--- routes.js
+++ routes.js
@@ -15,26 +15,26 @@ var appRouter = function(app){
              var i = 0;
               while(i<result.rows.length){
                       if(result.rows[i].rollno == req.body['username'] && result.rows[i].password == req.body['password']){
-                                  res.send(JSON.stringify({"name":result.rows[i].name,"status":"100"}));
+                                  res.end(JSON.stringify({"name":result.rows[i].name,"status":"100"}));
                                   console.log(JSON.stringify({"name":result.rows[i].name,"status":"100"}));
                                   i++;
                       }
                       else if(result.rows[i].password != req.body['password']){
-                                  res.send(JSON.stringify({"name":result.rows[i].name,"status":"50"}));
+                                  res.end(JSON.stringify({"name":result.rows[i].name,"status":"50"}));
                                   console.log(JSON.stringify({"name":result.rows[i].name,"status":"50"}));
                                   i++;
                       }
                       else{
-                                  res.send(JSON.stringify({"name":result.rows[i].name,"status":"0"}));
+                                  res.end(JSON.stringify({"name":result.rows[i].name,"status":"0"}));
                                   console.log(JSON.stringify({"name":result.rows[i].name,"status":"0"}));
                                   i++;
                       }
               }
 
               console.log("Found :)");
-              res.end();
+
            }
-              
+
            });
                });
        });

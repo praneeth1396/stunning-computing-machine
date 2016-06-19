b361a94563a422cd251218e04b9b8ad260b66ef0
Narasimha Praneeth <Narasimha Praneeth>
2016-06-19 17:45:12 +0530

    My project

--- routes.js
+++ routes.js
@@ -2,7 +2,7 @@ var pg = require('pg');
 var appRouter = function(app){
        app.get('/',function(req,res){
                 console.log("Hey");
-                res.snd("Hey");
+                res.send("Hey");
     pg.connect(process.env.DATABASE_URL, function(err, client, done) {
     client.query('SELECT * FROM softies2k14', function(err, result) {
          done();

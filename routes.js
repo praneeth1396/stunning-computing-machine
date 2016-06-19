d730b343ebd5bb4ae5b4fab02a4e505d793b4243
Narasimha Praneeth <Narasimha Praneeth>
2016-06-19 21:57:18 +0530

    My project

--- routes.js
+++ routes.js
@@ -13,24 +13,7 @@ var appRouter = function(app){
           }
            else{
              var i = 0;
-              while(i<result.rows.length){
-                      if(result.rows[i].rollno == req.body['username'] && result.rows[i].password == req.body['password']){
-                                  res.end(JSON.stringify({"name":result.rows[i].name,"status":"100"}));
-                                  console.log(JSON.stringify({"name":result.rows[i].name,"status":"100"}));
-                                  i++;
-                      }
-                      else if(result.rows[i].rollno == req.body['username'] && result.rows[i].password != req.body['password']){
-                                  res.end(JSON.stringify({"name":result.rows[i].name,"status":"50"}));
-                                  console.log(JSON.stringify({"name":result.rows[i].name,"status":"50"}));
-                                  i++;
-                      }
-                      else{
-                                  res.end(JSON.stringify({"name":result.rows[i].name,"status":"0"}));
-                                  console.log(JSON.stringify({"name":result.rows[i].name,"status":"0"}));
-                                  i++;
-                      }
-              }
-
+                   console.log(result.rows.rollno);
               console.log("Found :)");
 
            }

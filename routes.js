dc2ba30cc876f843ddf809c7739386be49d12a55
Narasimha Praneeth <Narasimha Praneeth>
2016-06-19 17:57:57 +0530

    My project

--- routes.js
+++ routes.js
@@ -5,13 +5,16 @@ var appRouter = function(app){
                 res.send("Hey");
     pg.connect(process.env.DATABASE_URL, function(err, client, done) {
     client.query('SELECT * FROM softies2k14', function(err, result) {
-         done();
+
          if (err){
             res.send("hey world");
+            console.log("hey world");
           }
            else{
               res.end(result +" found");
+              console.log("found");
            }
+           done();
            });
                });
        });

6abeaa98b807159bfd8ea7ba91f93493a8d089ad
Narasimha Praneeth <Narasimha Praneeth>
2016-06-19 21:33:42 +0530

    My project

--- routes.js
+++ routes.js
@@ -2,7 +2,6 @@ var pg = require('pg');
 var appRouter = function(app){
        app.post('/',function(req,res){
                 console.log("Hey");
-
                 console.log(req.body['username']);
 
     pg.connect(process.env.DATABASE_URL, function(err, client, done) {
@@ -35,7 +34,7 @@ var appRouter = function(app){
               console.log("Found :)");
               res.end();
            }
-              done();
+              
            });
                });
        });

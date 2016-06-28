9b713cf28dd814283ec70e4c949f2268622cb1b3
Narasimha Praneeth <Narasimha Praneeth>
2016-06-29 00:22:50 +0530

    My project

--- routes.js
+++ routes.js
@@ -47,7 +47,7 @@ var appRouter = function(app){
                                        else{
                                          array.push(JSON.stringify(result.rows[0]));
                                          console.log(JSON.stringify(result.rows[0]));
-                                         console.log(array.length);
+                                         console.log(array);
                                        }
                           });
                           i = i + 1;

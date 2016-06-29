f02402d8b57941048ff08e3c7fe8f128ca111e33
Narasimha Praneeth <Narasimha Praneeth>
2016-06-29 23:51:14 +0530

    My project

--- routes.js
+++ routes.js
@@ -49,7 +49,7 @@ var appRouter = function(app){
       var sem = req.body['stud_sem'];
 
       pg.connect(process.env.DATABASE_URL,function(err,client,done){
-           client.query('INSERT INTO student_details values('+rollno+','+sem+','+passwd+','+course+','+dept+','+sem+)'',function(err,result){
+           client.query('INSERT INTO student_details values('+rollno+','+sem+','+passwd+','+course+','+dept+','+sem+')',function(err,result){
                        if(err){
                          console.log("error!");
                          res.end("error!");
@@ -59,7 +59,7 @@ var appRouter = function(app){
                        }
            });
       });
-      
+
   });
   app.post('/login',function(req,res){
                 console.log(req.body);
